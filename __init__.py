8faec18224cdb00b6192abaf8c8e09c2415ab4a1
Sam Kingston <sam@sjkwi.com.au>
2012-01-23 07:48:26 +1000

    support providing header and footer as strings

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 import os
+import shutil
 import subprocess
+import tempfile
 
 class HTMLToPDFConverter(object):
     args_to_bin = {
@@ -14,16 +16,41 @@ class HTMLToPDFConverter(object):
         for k, v in kwargs.items():
             setattr(self.args_to_bin, k, v)
 
-    def convert(self, input_obj):
+    def convert(self, input_obj, header=None, footer=None):
+        if not os.path.isfile(self.path_to_bin):
+            raise Exception('wkhtmltopdf not found at %s' % self.path_to_bin)
+
+        temp_dir = tempfile.mkdtemp()
+        try:
+            r = self._convert(temp_dir, input_obj, header, footer)
+        except:
+            raise
+        finally:
+            shutil.rmtree(temp_dir)
+        return r
+
+    def _convert(self, temp_dir, input_obj, header, footer):
         # construct path for exec
         args = [self.path_to_bin, '-q']
         for k, v in self.args_to_bin.items():
             args.append('--%s' % k.replace('_', '-'))
             args.append(v)
-        args += ['-', '-'] # input from stdin, output to stdout
 
-        if not os.path.isfile(self.path_to_bin):
-            raise Exception('wkhtmltopdf not found at %s' % self.path_to_bin)
+        # write out header if it was given
+        if header is not None:
+            header_filename = os.path.join(temp_dir, 'header.html')
+            with open(header_filename, 'w') as fp:
+                fp.write(header)
+            args += ['--header-html', header_filename]
+
+        # write out footer if it was given
+        if footer is not None:
+            footer_filename = os.path.join(temp_dir, 'footer.html')
+            with open(footer_filename, 'w') as fp:
+                fp.write(footer)
+            args += ['--footer-html', footer_filename]
+
+        args += ['-', '-'] # input from stdin, output to stdout
 
         p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE)
